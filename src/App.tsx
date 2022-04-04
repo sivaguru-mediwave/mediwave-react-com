62f1270834bcc5ab120b755638d86d231fd40f51
sivaguru-mediwave <sivaguru@mindwaveventures.com>
2022-04-04 14:03:21 +0530

    react components initial setup completed

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='container'>
+      <div className="form-box">
+        {/* Header */}
+        <div className="header">
+          <h2>Form elements</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
