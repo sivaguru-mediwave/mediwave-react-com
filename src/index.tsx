524fc338b2de203d0f056f481bbb5fdd3a13bed6
deepak0987654321 <deepz.siva07@gmail.com>
2022-04-12 20:19:44 +0530

    tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './tailwind.css';
+import './typography.css';
 import App from './App';
-import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
     <App />,
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      white: "#ffffff",
+      dark: "#000000",
+      primary: "#5F227B",
+      secondary: "#A2C309"
+    },
+    fontFamily: {
+      base: ["Raleway", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
